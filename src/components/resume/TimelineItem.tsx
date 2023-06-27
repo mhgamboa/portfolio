0ebfe3370ad9d4b3bb0b5db5da443e1608f6d4cb
Marcus <marcus.gamboa@gmail.com>
2023-06-27 14:07:03 -0600

    Change from default import

--- src/components/resume/TimelineItem.tsx
+++ src/components/resume/TimelineItem.tsx
@@ -2,7 +2,7 @@ import { FC, memo } from "react";
 
 import { TimelineItem } from "./_resumedata";
 
-const TimeLineItem: FC<{ item: TimelineItem }> = memo(({ item }) => {
+export const TimeLineItem: FC<{ item: TimelineItem }> = memo(({ item }) => {
   const { title, date, location, content } = item;
   return (
     <div className="flex flex-col pb-8 text-center last:pb-0 md:text-left">
@@ -20,4 +20,3 @@ const TimeLineItem: FC<{ item: TimelineItem }> = memo(({ item }) => {
 });
 
 TimeLineItem.displayName = "TimeLineItem";
-export default TimeLineItem;
