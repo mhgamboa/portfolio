1f36751ada2f19bef49901286f7ba89d44f4004a
Marcus <marcus.gamboa@gmail.com>
2023-06-28 09:09:46 -0600

    Add padding to contact input

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -44,7 +44,7 @@ const ContactForm = memo(() => {
   };
 
   const inputClasses =
-    "bg-neutral-700 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-orange-600 rounded-md placeholder:text-neutral-400 placeholder:text-sm text-neutral-200 text-sm";
+    "p-1 bg-neutral-700 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-orange-600 rounded-md placeholder:text-neutral-400 placeholder:text-sm text-neutral-200 text-sm";
 
   return (
     <form
