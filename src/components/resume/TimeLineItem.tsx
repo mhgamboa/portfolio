4b448e0a73328df1cdf737d550812a0c861cd70d
Marcus <marcus.gamboa@gmail.com>
2023-07-05 00:22:59 -0600

    Change TimeLineItem mobile alignment

--- src/components/resume/TimeLineItem.tsx
+++ src/components/resume/TimeLineItem.tsx
@@ -5,12 +5,12 @@ import { TimelineItem } from "./_resumedata";
 export const TimeLineItem: FC<{ item: TimelineItem }> = memo(({ item }) => {
   const { title, date, location, content } = item;
   return (
-    <div className="flex flex-col pb-8 text-center last:pb-0 md:text-left">
+    <div className="flex flex-col pb-8 last:pb-0 text-left">
       <div className="flex flex-col pb-4">
         <h2 className="text-xl font-bold">{title}</h2>
         <div className="flex items-center justify-center gap-x-2 md:justify-start">
           <span className="flex-1 text-sm font-medium italic sm:flex-none">{location}</span>
-          <span>•</span>
+          <span>|</span>
           <span className="flex-1 text-sm sm:flex-none">{date}</span>
         </div>
       </div>
