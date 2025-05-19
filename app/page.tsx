9bbf2806541105b38c4c8d98cf61d67285da30f4
Marcus Gamboa <marcus.gamboa@gmail.com>
2025-05-19 12:48:39 -0600

    Update page.tsx
    Comment out Companies for now

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <Bio />
         <Services />
         <Projects />
-        <Companies />
+        {/* <Companies /> */}
         <ContactForm />
 
         {/* Footer */}
