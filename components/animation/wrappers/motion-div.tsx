496a3b2775b7798f66e4a6f4e8bd09ab55b8ff0a
Marcus Gamboa <marcus.gamboa@gmail.com>
2025-05-21 16:31:23 -0600

    Create motion-div.tsx
    It should be a typesafe wrapper that can be used to preserve SEO

---
+++ components/animation/wrappers/motion-div.tsx
@@ -0,0 +1,35 @@
+"use client";
+// This wrapper is used to preserve SEO for the page
+import React from "react";
+import { motion } from "framer-motion";
+import { AnimationProps, Transition, VariantLabels } from "framer-motion";
+
+interface Props {
+  children: React.ReactNode;
+  className?: string;
+  initial?: AnimationProps["initial"] | VariantLabels;
+  animate?: AnimationProps["animate"] | VariantLabels;
+  transition?: Transition;
+  variants?: AnimationProps["variants"];
+}
+
+export default function MotionDiv({
+  children,
+  className,
+  initial,
+  animate,
+  transition,
+  variants,
+}: Props) {
+  return (
+    <motion.div
+      className={className}
+      initial={initial}
+      animate={animate}
+      transition={transition}
+      variants={variants}
+    >
+      {children}
+    </motion.div>
+  );
+}
